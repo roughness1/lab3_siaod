41dde8353caf35b34152bb3789ebeb627325f34d
roughness1 <mikhail200720@gmail.com>
2026-04-25 11:41:29 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+
+class Queue:
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+
+    def enqueue(self, x):
+        self.in_stack.append(x)
+
+    def _transfer(self):
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+
+    def dequeue(self):
+        if self.size() == 0:
+            return None
+        
+        self._transfer()
+        return self.out_stack.pop()
+
+    def front(self):
+        if self.size() == 0:
+            return None
+            
+        self._transfer()
+        return self.out_stack[-1]
+
+    def size(self):
+        return len(self.in_stack) + len(self.out_stack)
